6ec98b93b0ce94fe13a4eec3c38cc631b7bbe782
Erwan <erwan.iquel@gmail.com>
2017-02-02 14:44:44 +0100

    Ajout des commentaire au programme shell1

--- tp2/include/shell1.h
+++ tp2/include/shell1.h
@@ -8,8 +8,21 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * Si échec de la lecture clavier vide stdin des caractère en trops
+ */
 static void purger();
+
+/**
+ * Supprime le caractère '\n' de la fin de saisie au clavier par fgets()
+ * @param string chaine saisie au clavier
+ */
 static void clean(char* string);
+
+/**
+ * fonction qui traite les commande envoyer par l'utilisateur
+ * @param commande commande à traiter dans l'environnement
+ */
 int commandthread(char* commande);
 
 #endif
