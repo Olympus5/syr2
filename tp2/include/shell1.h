3959bfe6935af5c222bacd24f36042cfeaa99aeb
Erwan <erwan.iquel@gmail.com>
2017-02-02 14:40:31 +0100

    Ajout du programme shell1.c

---
+++ tp2/include/shell1.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+static void purger();
+static void clean(char* string);
+int commandthread(char* commande);
+
+#endif
---
+++ tp2/src/shell1.c
@@ -0,0 +1,56 @@
+#include <shell1.h>
+
+int main(int argc, char* argv[])
+{
+  char commande[50];
+
+  while(1) {
+    printf("Entrez une commande: ");
+    fgets(commande, sizeof(commande), stdin);
+    clean(commande);
+    printf("%s\n", commande);
+
+    commandthread(commande);
+    break;
+  }
+
+  return 0;
+}
+
+static void clean(char* string) {
+  char* p = strchr(string, '\n');
+
+  if(p != NULL) {
+    *p = '\0';
+  } else {
+    purger();
+  }
+}
+
+static void purger() {
+  int c;
+
+  while((c = getchar()) != '\n' && c != EOF);
+}
+
+int commandthread(char* commande) {
+  int ret = -1, status;
+  pid_t pid = fork();
+
+  if(pid == -1) {
+    fprintf(stderr, "Erreur fork");
+    exit(1);
+  }
+
+  if(!strcmp(commande, "exit")) exit(0);
+
+  if(pid == 0) {
+    execlp(commande, commande, NULL);
+    ret = 1;
+  } else {
+    waitpid(-1, &status, 0);
+    ret = 1;
+  }
+
+  return ret;
+}
